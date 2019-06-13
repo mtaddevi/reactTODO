f8a4aec80ccad1bbdf9e4087a5e87f9be4f3ce9d
mtaddevi <mtaddevi@gmail.com>
2019-06-13 11:39:40 -0500

    Updates

---
+++ precache-manifest.1dfe8f89bef178bd3d8d0612490ac46e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3196db989f8cc17b1cf898c4ea54724",
+    "url": "/reactTODO/index.html"
+  },
+  {
+    "revision": "cb9ee5e51e94c7194543",
+    "url": "/reactTODO/static/css/main.d3079d3e.chunk.css"
+  },
+  {
+    "revision": "0d5738f6be9729c1e74c",
+    "url": "/reactTODO/static/js/2.ed1b6dbd.chunk.js"
+  },
+  {
+    "revision": "cb9ee5e51e94c7194543",
+    "url": "/reactTODO/static/js/main.31b3c93c.chunk.js"
+  },
+  {
+    "revision": "67ed17702ef54ee4be74",
+    "url": "/reactTODO/static/js/runtime~main.acbfdafa.js"
+  }
+]);
